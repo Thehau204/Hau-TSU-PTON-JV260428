102186baaaf5a5f3ea573b776862ce780024e404
Sancookie <113035834+Thehau204@users.noreply.github.com>
2026-05-26 13:36:13 +0900

    Ss10

---
+++ Session10/script/Ex03.js
@@ -0,0 +1,6 @@
+let width = 10;   // độ rộng
+let height = 5;   // chiều cao
+
+let area = width * height;
+
+console.log("Diện tích hình chữ nhật là:", area);
---
+++ Session10/script/Ex05.js
@@ -0,0 +1,7 @@
+let usd = prompt("Nhập số tiền USD:");
+
+usd = Number(usd);
+
+let vnd = usd * 25000;
+
+alert("Số tiền Việt Nam Đồng là: " + vnd + " đ");
